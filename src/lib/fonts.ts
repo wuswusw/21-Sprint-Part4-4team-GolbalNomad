2afd99ec6b35ca5007573b483343617231fab5ff
ljh815 <qnfn1315@gmail.com>
2026-03-11 07:53:11 +0900

    global.css 및 폰트 세팅, 구조 정리

---
+++ src/lib/fonts.ts
@@ -0,0 +1,18 @@
+import localFont from "next/font/local";
+
+export const pretendard = localFont({
+  src: [
+    {
+      path: "../../public/fonts/Pretendard-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Pretendard-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-pretendard",
+  display: "swap",
+});
