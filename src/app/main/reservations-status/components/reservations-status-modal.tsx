890690cbab78ce97a2cbcce1918178539c730f28
zzungju <zzungju07@gmail.com>
2026-03-17 15:35:22 +0900

    :sparkles: [KAN-20] Feat:reservations-status-modal 알림창 UI 구현 및 컴포넌트 분리

--- src/app/main/reservations-status/components/reservations-status-modal.tsx
+++ src/app/main/reservations-status/components/reservations-status-modal.tsx
@@ -1,7 +1,24 @@
+"use client"
+
+import { useState } from "react";
+
+import ReservationsStatusTabs from "./reservations-status-tabs";
+import ReservationsStatusItems from "./reservations-status-items";
+
+const TABS = ["신청", "승인", "거절"] as const;
+type Tab = typeof TABS[number];
+
 function ReservationsStatusModal() {
+    const [activeTab, setActiveTab] = useState<Tab>("신청");
+
     return (
-        <div>
-            <h1>모달 컴포넌트</h1>
+        <div className="w-85 px-6 py-[30px] bg-white rounded-3xl flex flex-col gap-[30px] shadow-[0_4px_24px_0_#9CB4CA33]">
+            <div className="flex flex-col gap-3 items-start relative">
+                <h2 className="text-20 font-bold">2026년 03월 17일</h2>
+                <img src="/assets/icons/delete.svg" alt="close" className="absolute top-0 right-0" />
+                <ReservationsStatusTabs tabList={TABS} activeTab={activeTab} setActiveTab={setActiveTab} />
+            </div>
+            <ReservationsStatusItems activeTab={activeTab} />
         </div>
     )
 }
