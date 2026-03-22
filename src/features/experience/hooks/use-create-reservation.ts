7a2925e303ba15b7be6a5cc53073d6bc791bd272
zzungju <zzungju07@gmail.com>
2026-03-22 14:59:57 +0900

    :sparkles:[KAN-25] Feat: 예약 성공 및 실패 시 alert-modal 연결

--- src/features/experience/hooks/use-create-reservation.ts
+++ src/features/experience/hooks/use-create-reservation.ts
@@ -1,20 +1,35 @@
+"use client";
+
 import { useMutation, useQueryClient } from "@tanstack/react-query";
-import {createReservation} from "../api/experience-detail.api"
+
+import { useModal } from "@/hooks/use-modal";
+import { createReservation } from "../api/experience-detail.api";
+import type { CreateReservationRequest } from "../types/experience-detail.type";
 
 export function useCreateReservation(activityId: number) {
-    const queryClient = useQueryClient();
-    return useMutation({
-        mutationFn: async(body: {scheduleId: number, headCount: number}) => {
-            return createReservation(activityId, body)},
-            onSuccess:(data) => {
-                alert(`예약이 완료되었습니다. 예약번호: ${data.id}`);
-                queryClient.invalidateQueries({ queryKey: ["my-reservations"] });
-                queryClient.invalidateQueries({ queryKey: ["activities", activityId, "available-schedule"] });
-            },
-            onError:(error, variables) => {
-                alert("예약에 실패했습니다.");
-                console.error("에러 발생 상세:", error);
-                console.log("실패한 예약 시도 데이터:", variables); // 테스트용
-            },
-    })
+  const queryClient = useQueryClient();
+  const { openModal } = useModal();
+
+  return useMutation({
+    mutationFn: (body: CreateReservationRequest) =>
+      createReservation(activityId, body),
+    onSuccess: (data) => {
+      openModal("alert", {
+        description: `예약이 완료되었습니다.`,
+        confirmText: "확인",
+      });
+      queryClient.invalidateQueries({ queryKey: ["my-reservations"] });
+      queryClient.invalidateQueries({
+        queryKey: ["activities", activityId, "available-schedule"],
+      });
+    },
+    onError: (error, variables) => {
+      openModal("alert", {
+        description: "예약에 실패했습니다.",
+        confirmText: "확인",
+      });
+      console.error("에러 발생 상세:", error);
+      console.log("실패한 예약 시도 데이터:", variables);
+    },
+  });
 }
