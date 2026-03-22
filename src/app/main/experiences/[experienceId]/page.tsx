da5f096104a8918a674b870edd0361c21aa1f361
zzungju <zzungju07@gmail.com>
2026-03-22 23:26:38 +0900

    :art:[KAN-25] Style: 반응형 예약 카드 슬라이드 연결 및 간단한 ui 구현

--- src/app/main/experiences/[experienceId]/page.tsx
+++ src/app/main/experiences/[experienceId]/page.tsx
@@ -84,7 +84,7 @@ function ExperienceDetailPage() {
 
   return (
     <div>
-            <div className="w-full mx-auto desktop:mt-22 tablet:mt-[34px] mt-[30px] desktop:mb-45 mb-[75px] mb-[30px] flex justify-center">
+            <div className="w-full mx-auto desktop:mt-22 tablet:mt-[34px] mt-[30px] desktop:mb-45 mb-[75px] mb-[30px] flex justify-center pb-[168px] desktop:pb-0">
                 <div className="w-full desktop:max-w-[1200px] px-10 grid grid-cols-1 desktop:grid-cols-[1fr_410px] gap-10">
                     <section className="w-full flex flex-col gap-6 tablet:gap-[30px] desktop:gap-10 text-gray-950">
                         <ImageGallery
@@ -133,16 +133,6 @@ function ExperienceDetailPage() {
                         />
                     </section>
                 </div>
-            </div>
-            <div className="w-full desktop:hidden sticky bottom-0 z-49 px-6 py-[18px] bg-white border-t border-[#e6e6e6]">
-                <div className="flex items-center justify-between mb-3">
-                <div className="flex justify-center items-center gap-1">
-                    <h2 className="text-18 font-bold">{experienceDetail?.price?.toLocaleString()}</h2>
-                    <p className="text-16 font-medium text-[#79747E]"> / 인</p>
-                </div>
-                <button className="text-16 font-bold text-primary-500 underline underline-offset-4">날짜 선택하기</button>
-                </div>
-                <button className="w-full px-5 py-4 rounded-[14px] bg-gray-200 text-white text-16 font-bold">예약하기</button>
             </div>
     </div>
   );
