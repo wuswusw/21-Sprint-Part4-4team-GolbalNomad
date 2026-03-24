5944c81d483c509adca1ea5920c18d37bfb05a99
zzungju <zzungju07@gmail.com>
2026-03-25 00:09:48 +0900

    :sparkles: [KAN-39] Feat: 내 체험리스트,월별 예약현황,날짜별 예약 정보가있는 스케줄,예약 시간대별 조회 목데이터연결

--- src/app/main/reservations-status/page.tsx
+++ src/app/main/reservations-status/page.tsx
@@ -7,14 +7,17 @@ import ReservationsStatusDetail from "@/features/reservation/components/reservat
 import Dropdown, { type DropdownItem } from "@/components/common/Dropdown";
 import { useMyActivities } from "@/features/reservation/hooks/use-my-activities";
 
+import { MOCK_MY_ACTIVITIES } from "@/features/reservation/reservations-status-mock-data"; //추후 삭제 예정
+
 function ReservationsStatusPage() {
     const {data, isLoading} = useMyActivities(100);
-    const activities = data?.pages.flatMap((page) => page.activities) ?? [];
+    const activities = MOCK_MY_ACTIVITIES; //추후 데이터 생기면 data?.pages.flatMap((page) => page.activities) ?? []; 로 변경
     const dropdownItems: DropdownItem[] = activities.map((activity) => ({
         id: activity.id.toString(),
         label:activity.title,
     }))
     const [selectedItem, setSelectedItem] = useState<DropdownItem | null>(null)
+    const [selectedDate, setSelectedDate] = useState<Date | null>(null)
 
     useEffect(() => {
         if(dropdownItems.length > 0 && !selectedItem) {
@@ -29,13 +32,13 @@ function ReservationsStatusPage() {
     return (
             <div className="desktop:w-160 tablet:w-[476px] w-full">
                 <div className="flex flex-col desktop:gap-[30px] tablet:gap-6 gap-[18px] text-gray-950">
-                    <div className="flex flex-col gap-[10px] tablet:px-0 px-6">
+                    <div className="flex flex-col gap-[10px] tablet:px-0 px-6 ">
                         <h2 className="text-18 font-bold">예약 현황</h2>
                         <p className="text-14 text-gray-500">내 체험에 예약된 내역들을 한 눈에 확인할 수 있습니다.</p>
                     </div>
                     {activities.length > 0 ? (
                         <div>
-                            <div className="tablet:px-0 px-6">
+                            <div className="tablet:px-0 px-6 mb-[30px]">
                                 <Dropdown
                                     items={dropdownItems}
                                     selectedItem={selectedItem || {id: "", label: "체험을 선택해 주세요"}}
@@ -45,9 +48,11 @@ function ReservationsStatusPage() {
         
                             {selectedItem && (
                                 <div>
-                                    <ReservationsStatusCalendar />
-                                    <ReservationsStatusDetail />
-                                </div>
+                                    <ReservationsStatusCalendar
+                                        activityId={Number(selectedItem.id)}
+                                        selectedDate={selectedDate}
+                                        onDateChange={setSelectedDate}
+                                    />                                </div>
                             )}
                         </div>
                     ) : (
--- src/features/reservation/components/reservations-status-detail.tsx
+++ src/features/reservation/components/reservations-status-detail.tsx
@@ -1,24 +1,41 @@
 "use client"
 
-import { useState } from "react";
+import { useState, useRef } from "react";
+import { format } from "date-fns";
 
 import ReservationsStatusTabs from "./reservations-status-tabs";
 import ReservationsStatusItems from "./reservations-status-items";
+import  useOutsideClick from "@/hooks/use-click-outside";
 
 const TABS = ["신청", "승인", "거절"] as const;
 type Tab = typeof TABS[number];
 
-function ReservationsStatusDetail() {
-    const [activeTab, setActiveTab] = useState<Tab>("신청");
+interface ReservationsStatusDetailProps {
+    activityId: number;
+    selectedDate: Date | null;
+    onClose: () => void;
+}
 
+function ReservationsStatusDetail({ activityId, selectedDate, onClose }: ReservationsStatusDetailProps) {
+    const [activeTab, setActiveTab] = useState<Tab>("신청");
+    void activityId;
+    
+    const detailRef = useRef<HTMLDivElement>(null);
+    useOutsideClick(detailRef, onClose, true);
+    
     return (
-        <div className="desktop:w-85 w-full px-6 py-[30px] bg-white rounded-3xl flex flex-col gap-[30px] shadow-[0_4px_24px_0_#9CB4CA33]">
+        <div ref={detailRef} className="desktop:w-85 w-full px-6 py-[30px] bg-white rounded-3xl flex flex-col gap-[30px] shadow-[0_4px_24px_0_#9CB4CA33] text-gray-950">
             <div className="flex flex-col gap-3 items-start relative">
-                <h2 className="text-20 font-bold">2026년 03월 15일</h2>
-                <img src="/assets/icons/delete.svg" alt="close" className="absolute top-0 right-0 cursor-pointer desktop:block hidden" />
+                <h2 className="text-20 font-bold">
+                    {selectedDate ? format(selectedDate, "yyyy년 MM월 dd일") : "날짜를 선택해 주세요"}
+                </h2>
+                <img src="/assets/icons/delete.svg" alt="close" onClick={onClose} className="absolute top-0 right-0 cursor-pointer desktop:block hidden" />
                 <ReservationsStatusTabs tabList={TABS} activeTab={activeTab} setActiveTab={setActiveTab} />
             </div>
-            <ReservationsStatusItems activeTab={activeTab} />
+            <ReservationsStatusItems 
+            activityId={activityId} 
+            selectedDate={selectedDate || new Date()}
+            activeTab={activeTab} />
         </div>
     )
 }
