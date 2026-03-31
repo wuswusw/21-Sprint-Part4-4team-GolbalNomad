d474120d4eee8882474df528d428123a418fc53d
You JiHyun <youjihyun@Youui-MacBookAir.local>
2026-04-01 03:45:12 +0900

    feature: 등록 페이지 기능 구현 및 반응형 작업

--- src/components/common/activities/image-upload-section.tsx
+++ src/components/common/activities/image-upload-section.tsx
@@ -26,14 +26,14 @@ export default function ImageUploadSection({
   const LABEL_COMMON_CLASSES = "mb-4 text-16 font-bold text-gray-950";
   
   const UPLOAD_BOX_CLASSES = `
-    h-40 w-40 cursor-pointer rounded-2xl 
+    h-[128px] w-[128px] cursor-pointer rounded-2xl 
     border border-[#E0E0E5] 
     flex flex-col items-center justify-center 
     bg-transparent hover:bg-gray-50 transition-all
   `;
 
   const PREVIEW_CONTAINER_CLASSES = `
-    relative h-40 w-40 rounded-2xl 
+    relative h-[128px] w-[128px] rounded-2xl 
     group overflow-hidden flex-shrink-0
   `;
 
@@ -48,12 +48,12 @@ export default function ImageUploadSection({
 
   return (
     <div className="flex flex-col gap-10">
-      <div className="flex flex-col gap-6">
-        <label className={LABEL_COMMON_CLASSES}>배너 이미지</label>
+      <div className="flex flex-col">
+        <label className={LABEL_COMMON_CLASSES}>배너 이미지 등록</label>
         <div className="flex flex-wrap gap-4 items-center">
           {!bannerImg && (
             <div onClick={() => bannerInputRef.current?.click()} className={UPLOAD_BOX_CLASSES}>
-              <Image src="/assets/icons/editButton.svg" alt="Upload" width={48} height={48} />
+              <Image src="/assets/icons/editButton.svg" alt="Upload" width={32} height={32} />
               <p className="mt-2 text-14 text-[#787486]">이미지 등록</p>
             </div>
           )}
@@ -79,11 +79,11 @@ export default function ImageUploadSection({
         </div>
       </div>
 
-      <div className="flex flex-col gap-6">
-        <label className={LABEL_COMMON_CLASSES}>소개 이미지</label>
+      <div className="flex flex-col">
+        <label className={LABEL_COMMON_CLASSES}>소개 이미지 등록</label>
         <div className="flex flex-wrap gap-4">
           <div onClick={() => introInputRef.current?.click()} className={UPLOAD_BOX_CLASSES}>
-            <Image src="/assets/icons/editButton.svg" alt="Add" width={48} height={48} />
+            <Image src="/assets/icons/editButton.svg" alt="Add" width={32} height={32} />
             <p className="mt-2 text-14 text-[#787486]">{introImgs.length}/4</p>
           </div>
           <input type="file" ref={introInputRef} onChange={handleIntroChange} accept="image/*" multiple className="hidden" />
